5f181fb0a8139089bd0f133c5b6ee1101bc26b86
JaskarnChohan <88167840+JaskarnChohan@users.noreply.github.com>
2024-10-10 19:52:01 +1300

    Updated Delete Jobs Function
    Fixed the function so now it removes all saved jobs and all applications that were connected to the job.

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -1,5 +1,7 @@
 const jobListing = require("../models/jobListing.js");
 const application = require("../models/application.js");
+const Profile = require("../models/profile"); // Import the Profile model
+const Application = require("../models/application"); // Import the Application model
 const { validationResult, check } = require("express-validator");
 const mongoose = require("mongoose");
 
@@ -139,11 +141,20 @@ exports.deleteJobListing = async (req, res) => {
       return res.status(404).json({ msg: "Job not found" });
     }
 
+    // Remove the job ID from all profiles' savedJobs
+    await Profile.updateMany(
+      { savedJobs: _id }, // Find profiles where savedJobs contains the job ID
+      { $pull: { savedJobs: _id } } // Remove the job ID from savedJobs
+    );
+
+    // Delete all applications related to the job
+    await Application.deleteMany({ jobId: _id });
+
     res.json({ msg: "Job successfully deleted!" });
   } catch (err) {
     // Handle server error
     console.error(err.message);
-    res.status(404).json({ errors: [{ msg: "Server error" }] });
+    res.status(500).json({ errors: [{ msg: "Server error" }] }); // Use 500 for server errors
   }
 };
 
